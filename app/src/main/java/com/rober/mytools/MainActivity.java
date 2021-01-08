9440d76df2b553551cc9388bd7d82957bf0d8a98
Enzo <guridienzo@gmail.com>
2021-01-08 10:20:03 -0300

    Se agregaron listeners en los botones

--- app/src/main/java/com/rober/mytools/MainActivity.java
+++ app/src/main/java/com/rober/mytools/MainActivity.java
@@ -2,14 +2,26 @@ package com.rober.mytools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.rober.mytools.Actividades.ActividadHerramientas;
+import com.rober.mytools.Fragmentos.Frg_Menu;
+import com.rober.mytools.Interfaces.Comunica_Menu;
+
+public class MainActivity extends AppCompatActivity implements Comunica_Menu {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public void mostrarMenu(int x_boton) {
+        Intent in=new Intent(this, ActividadHerramientas.class);
+        in.putExtra("BOTONPULSADO", x_boton);
+        startActivity(in);
     }
 }
